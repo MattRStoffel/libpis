c3907a0a41fbde0535276e33fa59220132e7cfef
jbehroz <josephbehroz@gmail.com>
2025-04-22 16:40:15 -0700

    added real sensor reading

--- main.c
+++ main.c
@@ -1,20 +1,33 @@
+#include "DirectRegister.h"
 #include "pid.h"
-#include "sim.h"
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LINE_SENS_1 23
+#define LINE_SENS_2 24
+#define LINE_SENS_3 25
+
+void read_sensors(int8_t sensors[3]) {
+  sensors[0] = getGPIO(LINE_SENS_1);
+  sensors[0] = getGPIO(LINE_SENS_2);
+  sensors[0] = getGPIO(LINE_SENS_3);
+}
+
 int main() {
-  // 1. Reset sim and PID
-  sim_init();
+  // 1. PID and GPIO setup
   PID_Controller pid;
   pid_init(&pid, 23.0, 0.00, 25.4, -100.0, 100.0);
+  setup_io();
+  initGPIO(LINE_SENS_1, GPIO_INPUT);
+  initGPIO(LINE_SENS_2, GPIO_INPUT);
+  initGPIO(LINE_SENS_3, GPIO_INPUT);
 
   // 2. Run simulation
   double total_error = 0;
   for (int i = 0; i < 19000; ++i) {
     int8_t sensors[3];
-    sim_read_sensors(sensors);
+    read_sensors(sensors);
     double known_error = 0;
     if (sensors[0])
       known_error = 1;
@@ -25,9 +38,7 @@ int main() {
 
     total_error += fabs(known_error);
     float diff = pid_update(&pid, known_error);
-    sim_drive_motors(diff, 0.7);
   }
 
-  sim_deinit();
   return 0;
 }
