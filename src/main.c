f798002e3b59d1858d936fe91589896d8d931a78
MattRStoffel <Matt@MrStoffel.com>
2025-04-22 21:05:00 -0700

    feet: pis

--- src/main.c
+++ src/main.c
@@ -25,12 +25,66 @@ void read_sensors(bool sensors[3]) {
   sensors[2] = get_gpio(LINE_SENS_3);
 }
 
+float calculate_error(const bool sensors[3]) {
+  const float weights[3] = {1.0, 0.0, -1.0};
+  float sum = 0;
+  int active_sensors = 0;
+
+  for (int i = 0; i < 3; ++i) {
+    if (sensors[i]) {
+      sum += weights[i];
+      active_sensors++;
+    }
+  }
+
+  if (active_sensors == 0)
+    return 0.0;
+
+  return sum / active_sensors;
+}
+
+void set_motor_speed(float pid_output) {
+  int max_speed = 100;
+  int sharp_thresh = 70;
+  int norm_thresh = 2;
+
+  int left_speed = 0, right_speed = 0;
+  char left_dir = FORWARD;
+  char right_dir = FORWARD;
+
+  // Turning right
+  if (pid_output > norm_thresh) {
+    left_speed = max_speed;
+    if (pid_output > sharp_thresh) { // Big correction: reverse right motor
+      right_dir = BACKWARD;
+      right_speed = (int)(pid_output);
+    } else {
+      right_speed = max_speed - (int)(pid_output);
+    }
+  }
+  // Turning left
+  else if (pid_output < -norm_thresh) {
+    right_speed = max_speed;
+    if (pid_output < -sharp_thresh) { // Big correction: reverse left motor
+      left_dir = BACKWARD;
+      left_speed = (int)(-pid_output);
+    } else {
+      left_speed = max_speed - (int)(-pid_output);
+    }
+  }
+  // No correction needed
+  else {
+    left_speed = max_speed;
+    right_speed = max_speed;
+  }
+
+  RunMotor(MOTOR_A, left_dir, left_speed);
+  RunMotor(MOTOR_B, right_dir, right_speed);
+}
+
 int main() {
-  PID_Controller pid;
-  pid_init(&pid, 23.0, 0.02, 25.4, -100.0, 100.0);
 
   setup_gpio();
-
   init_gpio(LINE_SENS_1, GPIO_INPUT);
   init_gpio(LINE_SENS_2, GPIO_INPUT);
   init_gpio(LINE_SENS_3, GPIO_INPUT);
@@ -40,30 +94,15 @@ int main() {
   signal(SIGINT, cancel_handler);
   signal(SIGTERM, cancel_handler);
 
-  // 2. Run simulation
+  PID_Controller pid;
+  pid_init(&pid, 50.0, 0.0, 290, -100.0, 100.0);
+
   while (running) {
     bool sensors[3];
-
     read_sensors(sensors);
-    double known_error = 0;
-
-    if (sensors[0]) {
-      known_error = 1;
-    } else if (sensors[2]) {
-      known_error = -1;
-    } else if (sensors[1]) {
-      known_error = 0;
-    }
-
+    double known_error = calculate_error(sensors);
     float diff = pid_update(&pid, known_error);
-
-    if (diff < 0.5) {
-      RunMotor(MOTOR_A, FORWARD, 100 * diff);
-      RunMotor(MOTOR_B, FORWARD, 200 * diff);
-    } else {
-      RunMotor(MOTOR_A, FORWARD, 200 * diff);
-      RunMotor(MOTOR_B, FORWARD, 100 * diff);
-    }
+    set_motor_speed(diff);
   }
 
   StopMotor();
