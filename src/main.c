6ad4bbbb759b53eb592d4054497dc28d66328d38
jbehroz <josephbehroz@gmail.com>
2025-04-22 18:10:54 -0700

    more fix

--- src/main.c
+++ src/main.c
@@ -58,11 +58,11 @@ int main() {
     float diff = pid_update(&pid, known_error);
 
     if (diff < 0.5) {
-      RunMotor(MOTOR_A, FORWARD, 1 * diff);
-      RunMotor(MOTOR_B, FORWARD, 2 * diff);
+      RunMotor(MOTOR_A, FORWARD, 100 * diff);
+      RunMotor(MOTOR_B, FORWARD, 200 * diff);
     } else {
-      RunMotor(MOTOR_A, FORWARD, 2 * diff);
-      RunMotor(MOTOR_B, FORWARD, 1 * diff);
+      RunMotor(MOTOR_A, FORWARD, 200 * diff);
+      RunMotor(MOTOR_B, FORWARD, 100 * diff);
     }
   }
 
